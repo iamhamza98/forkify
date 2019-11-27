d75db064759519504f71503cf04ca96d97071a11
Nicole <heeyeons1993@gmail.com>
2019-11-26 23:32:17 -0800

    Completed getting recipe list in to the UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,32 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
+
 
 const state = {};
 
 const controlSearch = async () => {
   // 1. Get query from view
-  const query = "pizza" // TODO from User side
+  const query = searchView.getInput(); // TODO from User side
+  //console.log(query);
 
+  if(query){
   // 2. New Search Object and add to the state
   state.search = new Search(query);
 
   // 3. Prepare UI for result (loading sign or delete previous result etc.)
+  searchView.clearInput();
+  searchView.clearResList();
 
   // 4. Search for recipes
   await state.search.getResults();
 
   // 5. Render result on UI
-  console.log(state.search.recipes);
+  searchView.renderResult(state.search.recipes);
+  }
 }
 
-document.querySelector('.search').addEventListener('submit', e=>{
+elements.searchForm.addEventListener('submit', e=>{
   e.preventDefault();
   controlSearch();
 })
